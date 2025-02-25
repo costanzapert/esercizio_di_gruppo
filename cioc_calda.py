b212805b7bf305ee9dac8689a337303ef5688735
costanzapert <costanzapertosa97@gmail.com>
2025-02-25 16:01:54 +0100

    Cioccolata Calda

--- cioc_calda.py
+++ cioc_calda.py
@@ -1,14 +1,18 @@
 from branc_main import Cioccolato
 
 class CioccolataCalda(Cioccolato):
-    def __init__(self, tipo, percentuale, temperatura, densità):
+    def __init__(self, tipo, percentuale, temperatura, densità, quantità):
         super().__init__(tipo, percentuale)
         self.temperatura = temperatura
         self.densità = densità
-        
-    def produce(self, quantità):
         if quantità >= 20:
-            print(f" Il tipo di cioccolato è: {self.tipo} e la percentuale di cacao è: {self.percentuale} ed usa {quantità} unità")
-        else:
-            print("Quantità non sufficiente")
+            self.quantità = quantità
+        else: 
+            self.quantità = 20
+        
+
+        
+    def produce(self):
+        print(f" Il tipo di cioccolato è: {self.tipo} e la percentuale di cacao è: {self.percentuale} ed usa {self.quantità} unità")
+    
 
